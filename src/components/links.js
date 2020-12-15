8a898b091dd7abc3106211211b141db1e78f5a69
jacoberick <jacobewhitedev@gmail.com>
2020-12-15 13:38:29 -0600

    phone number color change, bit.ly added for testimonials

--- src/components/links.js
+++ src/components/links.js
@@ -18,7 +18,7 @@ const Links = ({ burger, setBurger, active, refs }) => {
     <div>
       <a
         className="links"
-        href="https://www.google.com/search?rlz=1C5CHFA_enUS921US921&ei=Vu--X8mqG-j25gKmurLYBQ&q=Home-Aid+Moving%2C+LLC%2C+Spring+Hill%2C+TN&oq=Home-Aid+Moving&gs_lcp=CgZwc3ktYWIQARgBMgkIABDJAxAWEB4yAggmOgQIABBHOg0ILhDHARCvARBDEJMCOgYIABAKEEM6CwguEMcBEK8BEJECOgUIABCRAjoNCC4QsQMQxwEQowIQQzoECAAQQzoECC4QQzoLCC4QsQMQxwEQowI6CAguELEDEIMBOggIABCxAxCRAjoOCC4QsQMQgwEQxwEQowI6BQgAEMkDOgUIABCSAzoNCC4QxwEQowIQQxCTAjoKCC4QxwEQowIQQzoCCAA6CAguEMcBEK8BOgQIABANOgoILhDHARCvARANOgQIABAeOgYIABAKEB46CQgAEMkDEAoQHjoGCAAQFhAeOggIABAWEAoQHlDFX1jTfGD5kQFoAnAEeACAAXGIAdELkgEEMTQuM5gBAKABAaoBB2d3cy13aXrIAQjAAQE&sclient=psy-ab#lrd=0x886379bf40428dc7:0xab0feda87a6d2eee,1,,,"
+        href="http://bit.ly/3raecIG"
         target="_blank"
         rel="noreferrer"
       >
